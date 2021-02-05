2ca9f10917d062253f9e77d754230c067b47f265
Alistair Vu <alistairvu@gmail.com>
2021-02-05 23:56:10 +0700

    Enfore one card a day

--- screens/TodayScreen.tsx
+++ screens/TodayScreen.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import { ResultsScreen } from "./ResultsScreen"
 import { TodayHome } from "./TodayHome"
+import { Dimensions } from "react-native"
 
 const Stack = createStackNavigator()
 
@@ -9,7 +10,15 @@ export const TodayScreen = () => {
   return (
     <Stack.Navigator initialRouteName="Home" headerMode="none">
       <Stack.Screen name="Home" component={TodayHome} />
-      <Stack.Screen name="Result" component={ResultsScreen} />
+      <Stack.Screen
+        name="Result"
+        component={ResultsScreen}
+        options={{
+          gestureResponseDistance: {
+            horizontal: Dimensions.get("window").width,
+          },
+        }}
+      />
     </Stack.Navigator>
   )
 }
