b3b8a0fd42e1b85b6fc3e72b0508ba7268c1ef9b
Alistair Vu <alistairvu@gmail.com>
2021-02-05 23:56:01 +0700

    Add keyboard avoiding

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import { SearchHome } from "./SearchHome"
 import { ResultsScreen } from "./ResultsScreen"
+import { Dimensions } from "react-native"
 
 const Stack = createStackNavigator()
 
@@ -9,7 +10,15 @@ export const SearchScreen = () => {
   return (
     <Stack.Navigator initialRouteName="Home" headerMode="none">
       <Stack.Screen name="Home" component={SearchHome} />
-      <Stack.Screen name="Result" component={ResultsScreen} />
+      <Stack.Screen
+        name="Result"
+        component={ResultsScreen}
+        options={{
+          gestureResponseDistance: {
+            horizontal: Dimensions.get("window").width,
+          },
+        }}
+      />
     </Stack.Navigator>
   )
 }
